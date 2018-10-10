7a75d4ac7f09330884cd9f55892bca29d48a91cd
Jordan Dale <jordandale@Roberts-MacBook-Pro-3.local>
2018-10-10 17:54:55 -0400

    Changed to rows and added IDs

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ $('.mainButton').click(function() {
     return (clicked ? document.querySelector('.optionsContainer').style.display = "flex" : document.querySelector('.optionsContainer').style.display = "none")
 })
 
+// Change them from default to dark
+var clicks = 0
 $('#darkOrLightTheme').click(function() {
-    document.querySelector(".optionsContainer > #darkOrLightTheme").innerHTML = `Light Theme`
-    document.body.style.background = "black"
-    document.querySelector('h1').style.color = "white"
-    document.querySelector('.gameContainer').style.border = "5px solid darkblue"
-    document.querySelector('.gameContainer').style.background = "grey"
-})
+    if (clicks == 0) {
+        document.querySelector(".optionsContainer > #darkOrLightTheme").innerHTML = `Light Theme`
+        document.body.style.background = "black"
+        document.querySelector('h1').style.color = "blue"
+        document.querySelector('.gameContainer').style.border = "5px solid darkblue"
+        document.querySelector('.gameContainer').style.background = "grey"    
+    // Change back to light
+    } else {
+        document.body.style.background = "white"
+        document.querySelector('h1').style.color = "black"
+        document.querySelector('.gameContainer').style.border = "5px solid green"
+        document.querySelector('.gameContainer').style.background = "black"    
+        document.querySelector(".optionsContainer > #darkOrLightTheme").innerHTML = `Dark Theme`
+    }
+    ++clicks
+})  
